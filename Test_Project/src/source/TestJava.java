972117912f6613d97c944f38a557e40e42928672
Mohan Ram <Mohan Ram@MohanRam>
2020-06-07 20:55:23 +0530

    First java commit

---
+++ Test_Project/src/source/TestJava.java
@@ -0,0 +1,43 @@
+package source;
+
+public class TestJava {
+
+	public static void main(String[] args) 
+	{
+   	 
+int []a[][]=new int [2][][];
+
+System.out.println(a.getClass().getName());
+System.out.println(a);
+System.out.println(a[0]);
+System.out.println(a[1]);
+
+a[0] = new int [2][3];
+a[0][0][0] = 10;
+a[0][0][1] = 20;
+a[0][0][2] = 30;
+
+a[0][1][0] = 40;
+a[0][1][1] = 50;
+a[0][1][2] = 60;
+
+System.out.println(a[0][0][0]);
+System.out.println(a[0][1][2]);
+
+a[1] = new int [3][2];
+
+a[1][0][0] = 70;
+a[1][0][1] = 80;
+
+a[1][1][0] = 90;
+a[1][1][1] = 100;
+
+a[1][2][0] = 110;
+a[1][2][1] = 120;
+
+System.out.println(a[1][2][1]);
+
+
+	}
+
+}
